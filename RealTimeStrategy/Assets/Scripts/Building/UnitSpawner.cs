830f4f02ff92543dbc75ef00b15f8793197e0eef
Aditya Galang Adzana <adityagalang80@gmail.com>
2022-04-04 19:53:33 +0700

    Unit spawn queue

--- RealTimeStrategy/Assets/Scripts/Building/UnitSpawner.cs
+++ RealTimeStrategy/Assets/Scripts/Building/UnitSpawner.cs
@@ -1,14 +1,63 @@
 using Mirror;
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.EventSystems;
+using UnityEngine.UI;
 
 public class UnitSpawner : NetworkBehaviour, IPointerClickHandler
 {
     [SerializeField] private Health health = null;
-    [SerializeField] private GameObject unitPrefab = null;
+    [SerializeField] private Unit unitPrefab = null;
     [SerializeField] private Transform unitSpawnPoint = null;
+    [SerializeField] private TMP_Text remainingUnitsText = null;
+    [SerializeField] private Image unitProgressImage = null;
+    [SerializeField] private int maxUnitQueue = 5;
+    [SerializeField] private float spawnMoveRange = 7;
+    [SerializeField] private float unitSpawnDuration = 5f;
+
+    [SyncVar(hook = nameof(ClientHandleQueuedUnitsUpdated))]
+    private int queueUnits;
+    [SyncVar]
+    private float unitTimer;
+
+    private float progressImageVelocity;
+
+    private void Update()
+    {
+        if (isServer)
+        {
+            ProduceUnits();
+        }
+
+        if (isClient)
+        {
+            UpdateTimerDisplay();
+        }
+    }
+
+    [Server]
+    private void ProduceUnits()
+    {
+        if (queueUnits == 0) return;
+
+        unitTimer += Time.deltaTime;
+
+        if(unitTimer< unitSpawnDuration) { return; }
+
+        GameObject unitInstance = Instantiate(unitPrefab.gameObject, unitSpawnPoint.position, unitSpawnPoint.rotation);
+        NetworkServer.Spawn(unitInstance, connectionToClient);
+
+        Vector3 spawnOffset = Random.insideUnitSphere * spawnMoveRange;
+        spawnOffset.y = unitSpawnPoint.position.y;
+
+        UnitMovement unitMovement = unitInstance.GetComponent<UnitMovement>();
+        unitMovement.ServerMove(unitSpawnPoint.position + spawnOffset);
+
+        queueUnits--;
+        unitTimer = 0f;
+    }
 
     #region Server
 
@@ -25,9 +74,14 @@ public class UnitSpawner : NetworkBehaviour, IPointerClickHandler
     [Command]
     private void CmdSpawnUnit()
     {
-        GameObject unitInstance = Instantiate(unitPrefab, unitSpawnPoint.position, unitSpawnPoint.rotation);
+        if(queueUnits == maxUnitQueue) { return; }
+        RTSPlayer RTS = connectionToClient.identity.GetComponent<RTSPlayer>();
 
-        NetworkServer.Spawn(unitInstance, connectionToClient);
+        if(RTS.GetResources() < unitPrefab.GetResourceCost()) { return; }
+
+        queueUnits++;
+
+        RTS.SetResources(RTS.GetResources() - unitPrefab.GetResourceCost());
     }
 
     [Server]
@@ -40,6 +94,20 @@ public class UnitSpawner : NetworkBehaviour, IPointerClickHandler
 
     #region Client
 
+    private void UpdateTimerDisplay()
+    {
+        float newProgress = unitTimer / unitSpawnDuration;
+
+        if(newProgress < unitProgressImage.fillAmount)
+        {
+            unitProgressImage.fillAmount = newProgress;
+        }
+        else
+        {
+            unitProgressImage.fillAmount = Mathf.SmoothDamp(unitProgressImage.fillAmount, newProgress, ref progressImageVelocity, 0.1f);
+        }
+    }
+
     public void OnPointerClick(PointerEventData eventData)
     {
         Debug.Log("Masuk");
@@ -48,6 +116,11 @@ public class UnitSpawner : NetworkBehaviour, IPointerClickHandler
         if (!hasAuthority) { return; }
 
         CmdSpawnUnit();
+    }
+
+    private void ClientHandleQueuedUnitsUpdated(int oldUnits,int newUnits)
+    {
+        remainingUnitsText.text = newUnits.ToString();
     }
 
 
--- RealTimeStrategy/Assets/Scripts/Unit/UnitMovement.cs
+++ RealTimeStrategy/Assets/Scripts/Unit/UnitMovement.cs
@@ -47,13 +47,20 @@ public class UnitMovement : NetworkBehaviour
         agent.ResetPath();
     }
 
-    [Command]
-    public void CmdMove(Vector3 position)
+    [Server]
+    public void ServerMove(Vector3 position)
     {
         targeter.ClearTarget();
 
-        if (!NavMesh.SamplePosition(position,out NavMeshHit hit, 1f, NavMesh.AllAreas)) { return; }
+        if (!NavMesh.SamplePosition(position, out NavMeshHit hit, 1f, NavMesh.AllAreas)) { return; }
         agent.SetDestination(hit.position);
+    }
+
+
+    [Command]
+    public void CmdMove(Vector3 position)
+    {
+        ServerMove(position);
     }
 
     [Server]
